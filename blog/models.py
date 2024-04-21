7e9c0a8431c53f4d7759225d5e58351462951053
laflopet <laflopet@gmail.com>
2024-04-21 11:19:14 -0500

    Creacion de migraciones appblog

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250) # es igual a un varchar en bases de datos
-    slug = models.SlugField(max_length=250) # 
+    slug = models.SlugField(max_length=250) 
+    author = models.ForeignKey(User,   # relacion muchos a 1.  un post solo puede pertenecer a un usuario.
+                               on_delete=models.CASCADE, # se eliminan en cascada usuarios con post
+                               related_name='blog_posts') # verificar desde la clase de usuarios cuantos post a hecho.
+
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now) #fecha de publicacion actual
     created = models.DateTimeField(auto_now_add=True) #este campo se agrega cada ves que se crea un post
     updated = models.DateTimeField(auto_now=True) #este campo se agrega solo cada vez que se actualiza
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta(): # clases internas (se crean dentro de otra clase) se les llama metaclase
         ordering = ["-publish"] # ordenamiento inverso teniendo en cuenta la columna de publish
--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig', #activar la app de blog
 ]
 
 MIDDLEWARE = [
