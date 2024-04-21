ff1c6443150bba4ad43d00891a6f7b29b40a8acc
laflopet <laflopet@gmail.com>
2024-04-21 11:04:15 -0500

    Creacion de modelo para appblog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250) # es igual a un varchar en bases de datos
+    slug = models.SlugField(max_length=250) # 
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now) #fecha de publicacion actual
+    created = models.DateTimeField(auto_now_add=True) #este campo se agrega cada ves que se crea un post
+    updated = models.DateTimeField(auto_now=True) #este campo se agrega solo cada vez que se actualiza
+
+    class Meta(): # clases internas (se crean dentro de otra clase) se les llama metaclase
+        ordering = ["-publish"] # ordenamiento inverso teniendo en cuenta la columna de publish
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
+
+    def __str__(self):
+        return self.title
