b63621936ccf65ab83875c7c8e7be75b9d284513
laflopet <laflopet@gmail.com>
2024-04-24 10:02:25 -0500

    creacion de detalle y lista

--- blog/views.py
+++ blog/views.py
@@ -11,4 +11,4 @@ def post_list(request):
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
 
-    return render(request, 'blog/post/detail.html', {'posts': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
