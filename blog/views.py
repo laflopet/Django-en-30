1b34931767720439249739bafd63c9cb84e38cfd
laflopet <laflopet@gmail.com>
2024-04-25 15:23:54 -0500

    create pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
