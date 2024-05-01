a638b9fb467a9fb709790f95cae7f24a3e82d8c3
laflopet <laflopet@gmail.com>
2024-05-01 08:41:42 -0500

    correccion url

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ app_name = 'blog' # se crea para diferenciar la llamada de las url por el templa
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
-    path('<int:post_id>', views.post_share, name='post_share'),
-    path('<int:post_id>', views.post_comment, name='post_comment'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
