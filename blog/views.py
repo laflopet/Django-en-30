d38bf3853c100b8f4e8f50b8a3f468440d8394f2
laflopet <laflopet@gmail.com>
2024-05-01 15:57:00 -0500

    tags

--- blog/views.py
+++ blog/views.py
@@ -5,19 +5,26 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 #from mysite import settings
 
 # Create your views here.
-
+'''
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+'''
 
-def post_list(request):
+def post_list(request, tag_slug = None):
     posts_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     paginator = Paginator(posts_list, 3)
     page_number = request.GET.get('page', 1)
     
@@ -59,8 +66,16 @@ def post_detail(request, year, month, day, post):
                              )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags', '-publish')[:4]
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    return render(request, 'blog/post/detail.html', {'post': post, 
+                                                     'comments': comments, 
+                                                     'form': form,
+                                                     'similar_posts': similar_posts})
 
 @require_POST
 def post_comment(request, post_id):
